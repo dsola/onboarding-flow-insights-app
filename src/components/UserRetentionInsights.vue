c82192d1c9f3e7656eb6687b50039a0a59cfc367
David Solà <d.sola.03@gmail.com>
2019-10-20 22:47:14 +0200

    Make the retention curve with a lot of love

--- src/components/UserRetentionInsights.vue
+++ src/components/UserRetentionInsights.vue
@@ -8,8 +8,78 @@
     data () {
       return {
         chartOptions: {
+          title: {
+            text: 'Weekly retention curve of Temper boarding flow'
+          },
+          subtitle: {
+            text: 'Developed with love by David Sola for Temper Works'
+          },
+          yAxis: {
+            title: {
+              text: 'Percentage of retained users'
+            },
+            tickInterval: 5
+          },
+
+          xAxis: {
+            title: {
+              text: 'Percentage of Onboarding progress'
+            },
+            tickInterval: 5
+          },
+
           series: [{
-            data: [1,2,3] // sample data
+            name: '2016-09-09',
+            data: [
+              {
+                name: 'Create Account',
+                color: '#00FF00',
+                x: 0,
+                y: 100
+              },
+              {
+                name: 'Activate Account',
+                color: '#00FF00',
+                x: 20,
+                y: 100
+              },
+              {
+                name: 'Provide Profile Information',
+                color: '#00FF00',
+                x: 40,
+                y: 80
+              },
+              {
+                name: 'What jobs are you interested in?',
+                color: '#00FF00',
+                x: 50,
+                y: 60
+              },
+              {
+                name: 'Do you have relevant experience in these jobs?',
+                color: '#00FF00',
+                x: 70,
+                y: 40
+              },
+              {
+                name: 'Are you a freelancer?',
+                color: '#00FF00',
+                x: 90,
+                y: 40
+              },
+              {
+                name: 'Waiting for approval',
+                color: '#00FF00',
+                x: 99,
+                y: 40
+              },
+              {
+                name: 'Approval',
+                color: '#00FF00',
+                x: 100,
+                y: 40
+              },
+            ]
           }]
         }
       }
